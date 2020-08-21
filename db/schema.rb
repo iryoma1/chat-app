8a8d16b9c853a5712de96653382b1d42db537eab
iryoma1 <69664360+iryoma1@users.noreply.github.com>
2020-08-21 10:11:42 +0900

    room_usersテーブルのroom_usersテーブルの作成作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_093933) do
+ActiveRecord::Schema.define(version: 2020_08_20_095145) do
+
+  create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "name", null: false
